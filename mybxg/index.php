ea27f25c8ad4dcb41f3178315610b752d4c016f4
zhangpan <861841446@qq.com>
2017-09-19 11:25:28 +0800

    登录退出完成

--- mybxg/index.php
+++ mybxg/index.php
@@ -1,35 +1,29 @@
-<?php 
+﻿<?php 
+  // 后端路由（根据URL的不同响应不同的页面）
+  header('content-type:text/html; charset=utf8');
 
-     header('content-type:text/html;charset=utf8');
-   // 后端路由 （根据url的不同响应不同的页面）
-  // include 在当前的php中嵌入一个子页面
-  // 通过url区分出用户访问的页面
-   
-   // var_dump($_SERVER);
-
+  // 默认目录名称
+  $dir = 'main';
+  // 默认文件名称
+  $filename = 'index';
+  // 处理URL的路径
+  if(array_key_exists('PATH_INFO', $_SERVER)){
+    // PATH_INFO属性存在
+    // 获取请求路径
+    $path = $_SERVER['PATH_INFO']; // /main/index
+    // 去掉第一个斜杠
+    $str = substr($path, 1); // main/index
   
-   // echo $path;
-   // 默认文件目录
-   $dir='main';
-   // 默认文件名称
-   $filename='index';
-   // 判断path——info是否存在
-   if(array_key_exists('PATH_INFO', $_SERVER)){
-   	// path-info存在
-   	 $path = $_SERVER['PATH_INFO'];
-   	 // 去掉第一个/ 
-   	 $str = substr($path, 1);
-     // 字符串分割 
-     $ret=explode('/', $str);
-     if(count($ret)==2){
-        $dir = $ret[0];
-        $filename=$ret[1];
-     }else{
-     	 $filename='login';
-     }
-
-
-   };
-    include('./views/'.$dir.'/'.$filename.'.html');
+    $ret = explode('/', $str);
+    if(count($ret) === 2){
+      // 路由有两层
+      $dir = $ret[0];// 覆盖目录
+      $filename = $ret[1]; // 覆盖文件名称
+    }else{
     
-  ?>
+      $filename = 'login';
+    }
+  }
+
+  include('./views/'.$dir.'/'.$filename.'.html');
+?>
--- mybxg/public/js/common.js
+++ mybxg/public/js/common.js
@@ -5,4 +5,35 @@
 
 	$('.navs ul').prev('a').on('click', function () {
 		$(this).next().slideToggle();
-	});
+  });
+
+
+  // 点击退出按钮返回登录页面
+  $("#logoutBtn").click(function(){
+    $.ajax({
+      type:'post',
+      url:'/api/logout',
+      dataType:'json',
+      success:function(data){
+          if(data.code==200){
+            location.href="/main/login";
+          }
+      }
+    })
+  })
+// 检测用户是否登录
+
+var flag=$.cookie("PHPSESSID");
+// console.log(flag);
+// 如果没有flag 则用户没有登录，自动跳转到登录页面
+if(!flag){
+  location.href="/main/login";
+}
+
+// 设置头像信息
+var loginInfo=$.cookie('loginInfo');
+loginInfo=loginInfo&&JSON.parse(loginInfo);
+
+$(".aside .avatar img").attr("src",loginInfo.tc_avatar);
+$(".aside h4").html(loginInfo.tc_name);
+
