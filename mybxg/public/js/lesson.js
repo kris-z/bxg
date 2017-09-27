081c1dec14482ca2615e71d5bcef6cbcc04b714b
zhangpan <861841446@qq.com>
2017-09-27 09:25:45 +0800

    课时管理功能

--- mybxg/public/js/lesson.js
+++ mybxg/public/js/lesson.js
@@ -1,7 +1,7 @@
-define(['jquery','utils'],function($,utils){
+define(['jquery','utils',"template"],function($,utils,template){
   utils.setMenu('/course/course_add');
-  var csId=utils.qs('cs-id');
-
+  var csId=utils.qs('cs_id');
+  console.log(csId);
  $.ajax({
   type:'get',
   url:'/api/course/lesson',
@@ -9,6 +9,8 @@ define(['jquery','utils'],function($,utils){
   dataType:'json',
   success:function(data){
     console.log(data);
+    var html=template("lessonTpl",data.result);
+    $("#lessonInfo").html(html);
   }
  })
 })
